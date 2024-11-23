41516b761efa4116c88e18f432332764f4799a63
AzureAD\AdityaKumarDubey <aditya.dubey@urjobmart.com>
2024-11-23 21:39:36 +0530

    learn react router and make about page, home, layout page, contact page, footer, github page, header page, user page

--- 07reactrouter/src/index.js
+++ 07reactrouter/src/index.js
@@ -3,11 +3,37 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom';
+import Layout from './Layout.js'
+import Home from './Home/Home.js';
+import About from './About/About.js';
+import Contact from './components/Contact/Contact.js';
+import User from './components/User/User.js';
+import Github, {githubInfoLoader} from './components/Github/Github.js'
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<Layout />}>
+        <Route path='' element={<Home />} />
+        <Route path='about' element={<About />} />
+        <Route path='contact' element={<Contact />} />
+        <Route path='user/' element={<User />} >
+            <Route path=':userid' element={<User />} />
+        </Route>
+        <Route 
+        loader={githubInfoLoader}
+        path='github' 
+        element={<Github />} />
+        <Route path='*' element={<div>Not Found</div>} />
+    </Route> 
+  )
+)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <RouterProvider router={router}/>
   </React.StrictMode>
 );
 
