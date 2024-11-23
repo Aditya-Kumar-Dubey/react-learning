88d09aa43ff39f0ff6cc6c6933a364939cf15cbe
AzureAD\AdityaKumarDubey <aditya.dubey@urjobmart.com>
2024-11-23 22:46:16 +0530

    start learning context api

---
+++ 08minicontext/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import Login from './components/Login';
+import Profile from './components/Profile';
+
+function App() {
+  return (
+    <>
+    <h1>React video for context API</h1>
+    <Login />
+    <Profile />
+    </>
+  );
+}
+
+export default App;
